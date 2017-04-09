dddcc15476733ac71f56313f4a5b2128dab03fc6
Nathan Malishev <nathan.malishev@gmail.com>
2017-04-09 17:56:35 +1000

    testing redploy

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get('/healthcheck', (req, res) => {
   res.send('We are up and running')
 })
 
-app.get('/healthcheck1', (req, res) => {
-  res.send('healthcheck1 is this api endpoint')
+app.get('/healthcheck2', (req, res) => {
+  res.send('healthcheck2 izzzzs this api endpoint')
 })
 
 app.get('*', (req,res) => {
