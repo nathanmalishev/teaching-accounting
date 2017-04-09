d64a3219a6d142897080d2f48e85613996378f7f
Nathan Malishev <nathan.malishev@gmail.com>
2017-04-09 10:56:24 +1000

    basic api started

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+const express= require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.get('/healthcheck', (req, res) => {
+  res.send('We are up and running')
+})
+
+
+app.get('*', (req,res) => {
+  res.send('API request not found')
+})
+
+
+app.listen(PORT, () =>{
+  console.log('Server started on port ' + PORT)
+})
+
+
